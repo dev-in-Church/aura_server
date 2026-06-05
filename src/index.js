6edd52eef2e63f7feab91987873854b01146da20
HP-Probook <emmanuel19727@gmail.com>
2026-06-05 17:30:00 +0300

    add vercel and live url cors

--- src/index.js
+++ src/index.js
@@ -9,12 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+  "https://www.auraexpressafricaltd.com",
+  "https://aura-express-knjdlv97w-dev-in-churchs-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
+
 app.use(express.json());
 
 // Routes
